d93655cf53ea43f252a9b0128177e0e1e4e00ce3
yingpengsha <37143265+yingpengsha@users.noreply.github.com>
2018-04-10 09:57:34 +0800

    改 - 项目答辩 - 页面样式更改
    rt

--- src/views/projectDefense/index.vue
+++ src/views/projectDefense/index.vue
@@ -1,34 +1,40 @@
 <template>
-  <el-card style="width:85%;margin:0 auto">
-      <el-table
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        prop="id"
-        label="编号"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="title"
-        label="项目标题"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="date"
-        label="项目日期">
-      </el-table-column>
-      <el-table-column
-        prop="introduction"
-        label="项目简介">
-      </el-table-column>
-       <el-table-column>
-          <template slot-scope="scope">
-             <el-button @click="handleClick(scope.row)" type="success">编辑</el-button>
-             <el-button type="danger">删除</el-button>
-         </template>
-      </el-table-column>
-    </el-table>
-  </el-card>
+  <div class="dashboard-editor-container">
+    <el-card style="width:85%;margin:0 auto">
+        <div slot="header" class="clearfix">
+          <span style="font-size:25px">项目答辩</span>
+        </div>
+        <el-table
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          prop="id"
+          label="编号"
+          width="60"
+          align="center">
+        </el-table-column>
+        <el-table-column
+          prop="title"
+          label="项目标题"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="项目日期">
+        </el-table-column>
+        <el-table-column
+          prop="introduction"
+          label="项目简介">
+        </el-table-column>
+        <el-table-column width="250" align="center">
+            <template slot-scope="scope">
+              <el-button @click="handleClick(scope.row)" type="success">编辑</el-button>
+              <el-button type="danger">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -53,5 +59,8 @@ export default {
 </script>
 
 <style>
-
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+}
 </style>
