e16be204bbb4d9739bb23a8807ef019992fc71cb
Pic <104079381@qq.com>
2018-04-11 15:36:47 +0800

    改 - api/otherImportant - 路由名字
    修改下api名字

--- src/api/otherImportant.js
+++ src/api/otherImportant.js
@@ -1,6 +1,6 @@
 import request from '@/utils/request'
 
-export function fetchList(query) {
+export function fetchListGreat(query) {
   return request({
     url: '/otherImportant/list',
     method: 'get',
