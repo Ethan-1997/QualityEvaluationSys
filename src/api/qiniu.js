497959b78d448b8dde2fb2f0a4a9822182c2ebac
SoulBiuBiuBiu <1172807506@qq.com>
2018-05-02 08:52:20 +0800

    改 登录bug修复

--- src/api/qiniu.js
+++ src/api/qiniu.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getToken() {
   return request({
-    url: global.host + '/getUpToken', // 假地址 自行替换
+    url: '/getUpToken', // 假地址 自行替换
     method: 'get'
   })
 }
